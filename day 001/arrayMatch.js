8d6aa09d1ddc63d7deb30d57e0118b90398e5e4f
Aamir Aziz <aamir.aziz220@gmail.com>
2023-08-01 19:27:39 +0530

    change in array match

--- day 001/arrayMatch.js
+++ day 001/arrayMatch.js
@@ -2,15 +2,30 @@ let a = [1, 2, 3, 4, 5, 6, 8, 2, 11, 11, 2]
 let d = [1, 2, 4, 5, 8, 9, 2, 11]
 
 const similarValue = (arr1, arr2) => {
+    let a = [...arr2]
     let result = []
-    arr1.forEach(v => {
-
-        let idx = arr2.indexOf(v)
-        if (idx >= 0) {
-            result.push(v)
-            arr2.splice(idx, 1)
-        }
-    })
+    if (arr1.length && a.length) {
+        arr1.forEach(v => {
+            let idx = a.indexOf(v)
+            if (idx >= 0) {
+                result.push(v)
+                a.splice(idx, 1)
+            }
+        })
+    }
     return result
 }
+
+
+
+
+const mutualFriendCount = (arr1, arr2) => {
+    const count = similarValue(arr1, arr2).length;
+    if (count > 0) {
+        return count + " mutual friend"
+    } else {
+        return "No Mutual Friend Found!"
+    }
+}
 console.log('similarValue', similarValue(a, d));
+console.log('similarValue', mutualFriendCount(a, d));
